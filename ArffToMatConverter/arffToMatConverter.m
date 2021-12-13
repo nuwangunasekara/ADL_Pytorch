a3da611422ab28893bf2851daeaa07c5ea6de186
Nuwan Gunasekara <ng198002@gmail.com>
2021-12-13 22:27:12 +1300

    Add ARRF to Matlab converter

---
+++ ArffToMatConverter/arffToMatConverter.m
@@ -0,0 +1,30 @@
+addpath("matlab2weka")
+
+
+javaaddpath("/Volumes/weka-3-8-4/weka-3-8-4/weka.jar")
+
+if wekaPathCheck()
+    disp('Weka.jar is loaded!')
+end
+
+dataset_dir = "/Users/ng98/Desktop/datasets/NEW/unzipped/"
+save_dir = "/Users/ng98/Desktop/datasets/NEW/mat/"
+% datasets = ["covtypeNorm" "epsilon_normalized.t_class_Nominal" "AGR_g" "real-sim.libsvm.class_Nominal_sparse" "elecNormNew" "SVHN.scale.t.libsvm.sparse_class_Nominal" "sector.scale.libsvm.class_Nominal_sparse" "RBF_m" ]
+% datasets = ["kdd99"]
+% datasets = ["gisette_scale_class_Nominal"]
+datasets = ["sector.scale.libsvm.class_Nominal_sparse"]
+% datasets = ["spam_corpus"]
+for datasetName = datasets
+    in_f = strcat(dataset_dir, strcat(datasetName,'.arff'))
+    out_f = strcat(save_dir, strcat(datasetName,'.mat'))
+
+    wekaOBJ = loadARFF(in_f)
+    
+    [mdata,featureNames,targetNDX,stringVals,relationName] = weka2matlab(wekaOBJ)
+    
+    data = mdata
+    save (sprintf('%s', out_f), 'data', '-v7.3')
+    clear data
+    clear mdata featureNames targetNDX stringVals relationName
+
+end
